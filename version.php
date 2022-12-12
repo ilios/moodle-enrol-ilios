7151a3dee29c263526d054d6d361299af01b86bc
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-12-12 14:51:34 -0800

    version and requirement bump to Moodle 4.1

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022103100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2022041200;        // Requires this Moodle version
+$plugin->version   = 2022121200;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022111800;        // Requires this Moodle version
 $plugin->component = 'enrol_ilios';     // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array(
-    'local_iliosapiclient' => 2022103100,
+    'local_iliosapiclient' => 2022121200,
 );
