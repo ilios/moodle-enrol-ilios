72e0bd9e7dd6f925b37923fed6c751b08d50502a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-02-23 12:41:23 -0800

    realign plugin requirements and version info with Moodle 5.1.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025022400;       // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2024100100;       // Requires this Moodle version.
+$plugin->version   = 2026022300;       // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2025092600;       // Requires this Moodle version.
 $plugin->component = 'enrol_ilios';     // Full name of the plugin (used for diagnostics).
-$plugin->release = 'v4.5';
-$plugin->supported = [405, 405];
+$plugin->release = 'v5.1';
+$plugin->supported = [501, 501];
 $plugin->maturity = MATURITY_STABLE;
