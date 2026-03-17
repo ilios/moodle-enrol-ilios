9d38b7eccd8b8186a812a7c38ab37df1459eb1f9
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-03-17 09:48:18 -0700

    minor version bump.

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025022400;       // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025022401;       // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100100;       // Requires this Moodle version.
 $plugin->component = 'enrol_ilios';     // Full name of the plugin (used for diagnostics).
-$plugin->release = 'v4.5';
+$plugin->release = 'v4.5.1';
 $plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
